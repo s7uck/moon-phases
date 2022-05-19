8371ecfdb1790864548b64396206d3e32d49a70a
s7uck <stuck@stuckduck.it>
2022-05-19 17:26:54 +0200

    Added header nav

--- web/script.js
+++ web/script.js
@@ -1,15 +1,16 @@
 const moon = document.getElementById('moon');
+const timestamp = document.getElementById('timestamp');
 const imgIndex = `https://svs.gsfc.nasa.gov/vis/a000000/a004800/a004874`;
 let phases = [
+	'phase_new.1026_print.jpg',
 	'phase_waxing_crescent.0398_print.jpg',
 	'phase_first_quarter.5440_print.jpg',
 	'phase_waxing_gibbous.4801_print.jpg',
 	'phase_full.3492_print.jpg',
 	'phase_waning_gibbous.2172_print.jpg',
 	'phase_third_quarter.2243_print.jpg',
-	'phase_waning_crescent.0903_print.jpg',
-	'phase_new.1026_print.jpg'
-]
+	'phase_waning_crescent.0903_print.jpg'
+];
 
 phases.forEach(
 	filename =>
@@ -36,3 +37,5 @@ let currentMoon = new Moon();
 (async function() {
 	moon.src = phases[await currentMoon.phase];
 })()
+
+timestamp.innerText = new Date().toDateString()
